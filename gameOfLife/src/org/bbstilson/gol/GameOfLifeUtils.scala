5858357fdd8d7e54748a62f24e13a520d51eee17
Brandon <bbstilson@fastmail.com>
2020-02-24 13:23:18 -0800

    make the cells wrap

--- gameOfLife/src/org/bbstilson/gol/GameOfLifeUtils.scala
+++ gameOfLife/src/org/bbstilson/gol/GameOfLifeUtils.scala
@@ -7,27 +7,14 @@ object GameOfLifeUtils {
     height: Int,
     cellSize: Int
   )(x: Int, y: Int): List[Vector2] = {
-    List(
-      // up
-      if (y - cellSize < 0) None else Some(Vector2(x, y - cellSize)),
-      // down
-      if (y + cellSize > (height - cellSize)) None else Some(Vector2(x, y + cellSize)),
-      // left
-      if (x - cellSize < 0) None else Some(Vector2(x - cellSize, y)),
-      // right
-      if (x + cellSize > (width - cellSize)) None else Some(Vector2(x + cellSize, y)),
-      // up left
-      if (x - cellSize < 0 || y - cellSize < 0) None
-      else Some(Vector2(x - cellSize, y - cellSize)),
-      // up right
-      if (x + cellSize > (width - cellSize) || y - cellSize < 0) None
-      else Some(Vector2(x + cellSize, y - cellSize)),
-      // down left
-      if (x - cellSize < 0 || y + cellSize > (height - cellSize)) None
-      else Some(Vector2(x - cellSize, y + cellSize)),
-      // down right
-      if (x + cellSize > (width - cellSize) || y + cellSize > (height - cellSize)) None
-      else Some(Vector2(x + cellSize, y + cellSize))
-    ).flatten
+    val up = Vector2(x, if (y - cellSize < 0) height - cellSize else y - cellSize)
+    val down = Vector2(x, (y + cellSize) % height)
+    val left = Vector2(if (x - cellSize < 0) width - cellSize else x - cellSize, y)
+    val right = Vector2((x + cellSize) % width, y)
+    val upLeft = up.copy(x = left.x)
+    val upRight = up.copy(x = right.x)
+    val downLeft = down.copy(x = left.x)
+    val downRight = down.copy(x = right.x)
+    List(up, down, left, right, upLeft, upRight, downLeft, downRight)
   }
 }
--- gameOfLife/test/src/org/bbstilson/gol/GameOfLifeUtilsSpec.scala
+++ gameOfLife/test/src/org/bbstilson/gol/GameOfLifeUtilsSpec.scala
@@ -5,24 +5,59 @@ import scalafx.scene.paint.Color._
 class GameOfLifeUtilsSpec extends UnitSpec {
   import GameOfLifeUtils._
 
-  "calcNeighborsWithScreenSizes" should "calculate neighbors" in {
-    val f = calcNeighborsWithScreenSizes(3, 3, 1)(_, _)
-    f(0, 0) shouldBe List(Vector2(0, 1), Vector2(1, 0), Vector2(1, 1))
+  val f = calcNeighborsWithScreenSizes(3, 3, 1)(_, _)
+
+  "calcNeighborsWithScreenSizes" should "calculate neighbors - top left corner" in {
+    f(0, 0) shouldBe List(
+      Vector2(0, 2),
+      Vector2(0, 1),
+      Vector2(2, 0),
+      Vector2(1, 0),
+      Vector2(2, 2),
+      Vector2(1, 2),
+      Vector2(2, 1),
+      Vector2(1, 1)
+    )
+  }
+  it should "calculate neighbors - top middle" in {
     f(1, 0) shouldBe List(
+      Vector2(1, 2),
       Vector2(1, 1),
       Vector2(0, 0),
       Vector2(2, 0),
+      Vector2(0, 2),
+      Vector2(2, 2),
       Vector2(0, 1),
       Vector2(2, 1)
     )
-    f(2, 0) shouldBe List(Vector2(2, 1), Vector2(1, 0), Vector2(1, 1))
+  }
+
+  it should "calculate neighbors - top right corner" in {
+    f(2, 0) shouldBe List(
+      Vector2(2, 2),
+      Vector2(2, 1),
+      Vector2(1, 0),
+      Vector2(0, 0),
+      Vector2(1, 2),
+      Vector2(0, 2),
+      Vector2(1, 1),
+      Vector2(0, 1)
+    )
+
+  }
+  it should "calculate neighbors - left middle" in {
     f(0, 1) shouldBe List(
       Vector2(0, 0),
       Vector2(0, 2),
+      Vector2(2, 1),
       Vector2(1, 1),
+      Vector2(2, 0),
       Vector2(1, 0),
+      Vector2(2, 2),
       Vector2(1, 2)
     )
+  }
+  it should "calculate neighbors - middle" in {
     f(1, 1) shouldBe List(
       Vector2(1, 0),
       Vector2(1, 2),
@@ -33,21 +68,56 @@ class GameOfLifeUtilsSpec extends UnitSpec {
       Vector2(0, 2),
       Vector2(2, 2)
     )
+
+  }
+  it should "calculate neighbors - right middle" in {
     f(2, 1) shouldBe List(
       Vector2(2, 0),
       Vector2(2, 2),
       Vector2(1, 1),
+      Vector2(0, 1),
       Vector2(1, 0),
-      Vector2(1, 2)
+      Vector2(0, 0),
+      Vector2(1, 2),
+      Vector2(0, 2)
     )
-    f(0, 2) shouldBe List(Vector2(0, 1), Vector2(1, 2), Vector2(1, 1))
+
+  }
+  it should "calculate neighbors - bottom left corner" in {
+    f(0, 2) shouldBe List(
+      Vector2(0, 1),
+      Vector2(0, 0),
+      Vector2(2, 2),
+      Vector2(1, 2),
+      Vector2(2, 1),
+      Vector2(1, 1),
+      Vector2(2, 0),
+      Vector2(1, 0)
+    )
+
+  }
+  it should "calculate neighbors - bottom middle" in {
     f(1, 2) shouldBe List(
       Vector2(1, 1),
+      Vector2(1, 0),
       Vector2(0, 2),
       Vector2(2, 2),
       Vector2(0, 1),
-      Vector2(2, 1)
+      Vector2(2, 1),
+      Vector2(0, 0),
+      Vector2(2, 0)
     )
-    f(2, 2) shouldBe List(Vector2(2, 1), Vector2(1, 2), Vector2(1, 1))
+  }
+  it should "calculate neighbors - bottom right corner" in {
+    f(2, 2) shouldBe List(
+      Vector2(2, 1),
+      Vector2(2, 0),
+      Vector2(1, 2),
+      Vector2(0, 2),
+      Vector2(1, 1),
+      Vector2(0, 1),
+      Vector2(1, 0),
+      Vector2(0, 0)
+    )
   }
 }
